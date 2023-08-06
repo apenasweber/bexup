40db4dcb7d5cc3d1c7cf0bf25e110f4c50196a06
Weber <andweber92@gmail.com>
2023-08-06 13:11:29 -0300

    test: insert unit tests for auth

--- tests/unit/test_auth_bearer.py
+++ tests/unit/test_auth_bearer.py
@@ -1,9 +1,28 @@
-from app.api.v1.auth.auth_bearer import AuthBearer
+# tests/unit/test_auth_bearer.py
+from fastapi import HTTPException
+import pytest
+
+# Sample valid and invalid tokens for testing
+VALID_TOKEN = "valid.token.for.testing"
+INVALID_TOKEN = "invalid.token.for.testing"
 
 
-def test_auth_bearer():
-    auth_bearer = AuthBearer()
+def test_verify_token_invalid(auth_bearer):
+    assert auth_bearer.verify_token(INVALID_TOKEN) is False
 
-    assert isinstance(auth_bearer.model, str)
-    assert auth_bearer.scheme_name == "Bearer"
-    assert auth_bearer.auto_error
+
+def test_verify_request_missing_token(auth_bearer, invalid_auth_request):
+    # Verify that the method raises an HTTPException with status code 401
+    with pytest.raises(HTTPException) as exc:
+        auth_bearer.verify_request(invalid_auth_request)
+    assert exc.value.status_code == 401
+
+
+def test_verify_request_invalid_token(auth_bearer, invalid_auth_request):
+    # Set an invalid token in the headers of the mock request
+    invalid_auth_request.headers["Authorization"] = f"Bearer {INVALID_TOKEN}"
+
+    # Verify that the method raises an HTTPException with status code 401
+    with pytest.raises(HTTPException) as exc:
+        auth_bearer.verify_request(invalid_auth_request)
+    assert exc.value.status_code == 401
